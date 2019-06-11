10951dceacbf0bc9b86415036fa17ec4543f4514
facu aballay <flacomza09@gmail.com>
2019-06-10 22:59:00 -0300

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -18,6 +18,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 //Base de datos
 //=====================
 
+ if (process.env.NODE_ENV === 'dev'){
+    urldb='mongodb://localhost:27017/cafe'
+ }else{
+     urldb=process.env.MONGO_URI;
+ }
 
 
 process.env.urldb=urldb;
