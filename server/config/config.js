dd4e2ec7013089e03733c7030f438167ed943383
facuaballay <50968199+facuaballay@users.noreply.github.com>
2019-06-10 22:49:19 -0300

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -18,11 +18,14 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 //Base de datos
 //=====================
 
+<<<<<<< HEAD
  if (process.env.NODE_ENV === 'dev'){
     urldb='mongodb://localhost:27017/cafe'
  }else{
      urldb=process.env.MONGO_URI;
  }
+=======
+>>>>>>> Update config.js
 
 
 process.env.urldb=urldb;
