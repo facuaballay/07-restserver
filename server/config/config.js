db3e395b222463c2c252b38572f7e6632c2f54d3
facuaballay <50968199+facuaballay@users.noreply.github.com>
2019-06-10 22:49:19 -0300

    Update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -18,11 +18,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 //Base de datos
 //=====================
 
- if (process.env.NODE_ENV === 'dev'){
-    urldb='mongodb://localhost:27017/cafe'
- }else{
-     urldb="mongodb+srv://facu:41112258@restserver-prueba-8s8wy.mongodb.net/cafe?retryWrites=true&w=majority";
- }
 
 
 process.env.urldb=urldb;
