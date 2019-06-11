0ad3a323548f01f4374fcc26a4d4b485fe7d8c13
facu aballay <flacomza09@gmail.com>
2019-06-10 23:07:13 -0300

    config

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
 app.use(require('./routes/usuario'));
-
+ 
 
 
 mongoose.connect(process.env.urldb, {useNewUrlParser: true,useCreateIndex:true});
